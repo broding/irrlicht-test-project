1fa1dc876ba9c0c1a7f485790be9cd3cfc3227f6
Stijn Groothuis <stijn.groothuis@hva.nl>
2013-02-21 23:50:22 +0100

    added vcxproj and small change to StandardObject,h

--- NerdyGame/NerdyGame/StandardObject.h
+++ NerdyGame/NerdyGame/StandardObject.h
@@ -1,11 +1,8 @@
-//=== point/point.h =================================================
-
 #ifndef STANDARDOBJECT_H
 #define STANDARDOBJECT_H
 class StandardObject {
     public:
         StandardObject();  // parameterless default constructor
-       // TestLevel(IVideoDriver* driver_,); //  constructor with parameters
         int getX();
         int getY();
 		void drawall();
