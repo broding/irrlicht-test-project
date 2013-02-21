7da5f5f78780a7a099ff90bc35c3e4e124cc4b57
Stijn Groothuis <stijn.groothuis@hva.nl>
2013-02-22 00:50:37 +0100

    cleaned up .h

--- NerdyGame/NerdyGame/StandardObject.h
+++ NerdyGame/NerdyGame/StandardObject.h
@@ -8,15 +8,6 @@ using namespace gui;
 #ifndef STANDARDOBJECT_H
 #define STANDARDOBJECT_H
 
-#include <irrlicht.h>
-
-using namespace irr;
-using namespace core;
-using namespace scene;
-using namespace video;
-using namespace io;
-using namespace gui;
-
 class StandardObject {
     public:
 		StandardObject();  // parameterless default constructor
