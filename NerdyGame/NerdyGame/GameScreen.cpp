1d98d2bed7e777f333a19b04625e0e8e5f9a266d
Hugo Mater <mater09@hva.nl>
2013-02-22 15:46:52 +0100

    changed the skydome picture

--- NerdyGame/NerdyGame/GameScreen.cpp
+++ NerdyGame/NerdyGame/GameScreen.cpp
@@ -9,7 +9,7 @@ GameScreen::GameScreen()
 {
 	StandardObject flag(vector3df(0,0,0));
 	
-	scene::ISceneNode* skydome=smgr->addSkyDomeSceneNode(driver->getTexture("../Assets/skydome.jpg"),64,48,0.999f,5.0f,1000.0f);
+	scene::ISceneNode* skydome=smgr->addSkyDomeSceneNode(driver->getTexture("../Assets/skydome2.jpg"),64,48,0.999f,5.0f,1000.0f);
 	
 	smgr->addCameraSceneNodeFPS();
 }
