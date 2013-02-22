9132cd5b3c5e7dd8d2afb8fc94817b614d9c8df1
Bas Roding <bas.roding@gmail.com>
2013-02-22 12:22:21 +0100

    removed all comments

--- NerdyGame/NerdyGame/GameScreen.cpp
+++ NerdyGame/NerdyGame/GameScreen.cpp
@@ -11,11 +11,6 @@ GameScreen::GameScreen()
 	
 	scene::ISceneNode* skydome=smgr->addSkyDomeSceneNode(driver->getTexture("../Assets/skydome.jpg"),64,48,0.999f,5.0f,1000.0f);
 	
-	/*
-	To look at the mesh, we place a camera into 3d space at the position
-	(0, 30, -40). The camera looks from there to (0,5,0), which is
-	approximately the place where our md2 model is.
-	*/
 	smgr->addCameraSceneNodeFPS();
 }
 
